836d799ecdc41a37845910e585795558ab3c02bf
lucianoalvarez22 <xianoalvarez@gmail.com>
2023-02-07 01:52:07 +0100

    actual

--- Proyecto_Eclipse/ejerciciosTemaCero/src/ejerciciosTemaCero/EjerciciosPropuestos.java
+++ Proyecto_Eclipse/ejerciciosTemaCero/src/ejerciciosTemaCero/EjerciciosPropuestos.java
@@ -18,10 +18,21 @@ public class EjerciciosPropuestos {
 	
 	public static void ejercicio8() {
 		
+		int resultado;
+		char letra;
+		boolean es_vocal;
+
+		resultado= (int)(Math.random()*26+65); //Sumamos al numero de letras (sin ñ) el valor en ASCII
+		letra = (char)resultado;
+		es_vocal = ((letra=='A') || (letra=='E') || (letra=='I') || (letra=='O') || (letra=='U'));
+
+		System.out.println("Letra obtenida: " + letra);
+		System.out.println("¿Es una vocal?: " + es_vocal );
+		System.out.println("¿Es una consonante?: " + !es_vocal);
 	}
 
 	public static void main(String[] args) {
-		ejercicio4();
+		ejercicio8();
 	}
 
 }
