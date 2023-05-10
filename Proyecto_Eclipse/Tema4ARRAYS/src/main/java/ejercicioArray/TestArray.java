c7f359584c0f53785cb09016727b252fc397803f
lucianoalvarez22 <xianoalvarez@gmail.com>
2023-05-11 00:11:29 +0200

    EjercicioArray Actualizacion

--- Proyecto_Eclipse/Tema4ARRAYS/src/main/java/ejercicioArray/TestArray.java
+++ Proyecto_Eclipse/Tema4ARRAYS/src/main/java/ejercicioArray/TestArray.java
@@ -1,36 +1,45 @@
 package ejercicioArray;
 
+import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.Scanner;
 
 public class TestArray {
-	
-	//Crear una constante
-	private static final int TAMANIO_ARRAY=5;
-	
-	
-	public static void main(String[] args) {
-		//Crear un array del tamaño de la constante
+
+	// Crear una constante
+	private static final int TAMANIO_ARRAY = 5;
+
+	public static void EjercicioArray() {
+		// Crear un array del tamaño de la constante
 		int[] myArray = new int[TAMANIO_ARRAY];
-		
-		 //pedir al usuario que inserte el numero de elementos segun la constante
-		Scanner entradaEscaner = new Scanner (System.in); 
-		System.out.println("Introduce " + TAMANIO_ARRAY + " numeros:");
-		for(int i=0; i<TAMANIO_ARRAY; i++) {
-			 //meter los elementos en el array
-			myArray[i] = entradaEscaner.nextInt();
+
+		// pedir al usuario que inserte el numero de elementos segun la constante
+		Scanner entradaEscaner = new Scanner(System.in);
+
+		for (int i = 0; i < TAMANIO_ARRAY; i++) {
+			System.out.print("Introduce un numero en la posicion " + i + ": ");
+			try {
+				// Leo lo que ingresa el usuario en forma de cadena y lo parseo a entero
+				int num = entradaEscaner.nextInt();
+				// guardo el entero en "myArray"
+				myArray[i] = num;
+			} catch (InputMismatchException e) { 
+				// Si meto otra cosa que no sea numero, salta este error y decremento la i para
+				// repetir la iteracion del bucle
+				System.out.println("Solo se permiten numeros enteros. Intentalo de nuevo.");
+				i--;
+		        entradaEscaner.next();
+			}
 		}
-		
-		 //mostrar el array entero por pantalla
+
+		// mostrar el array entero por pantalla
 		System.out.println("Array FINAL:");
-	    for (int i = 0; i < TAMANIO_ARRAY; i++) {
-	      System.out.print(myArray[i] + " ");
-	    }
-		
+		for (int i = 0; i < TAMANIO_ARRAY; i++) {
+			System.out.print(myArray[i] + " ");
+		}
 	}
-	
 
-	   
-
-	   //mostrar el array entero por pantalla
+	public static void main(String[] args) {
+		EjercicioArray();
+	}
 }
